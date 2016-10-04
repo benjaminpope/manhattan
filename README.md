041dac48ac5a11ea79c4676b35f23129d6ff30a1
benjaminpope <benjamin.pope@astro.ox.ac.uk>
2016-10-04 15:21:15 +0100

    setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # csperiodograms
+[![Licence](http://img.shields.io/badge/license-GPLv3-blue.svg?style=flat)](http://www.gnu.org/licenses/gpl-3.0.html)
+
+
 Test L1-norm Periodograms for Astronomy
---
+++ setup.py
@@ -0,0 +1,27 @@
+from numpy.distutils.core import setup, Extension
+from numpy.distutils.misc_util import Configuration
+import distutils.sysconfig as ds
+
+long_description = ''
+
+setup(name='csperiodograms',
+      version='0.5',
+      description='Compressed Sensing Periodograms',
+      long_description=long_description,
+      author='Benjamin Pope',
+      author_email='benjamin.pope@physics.ox.ac.uk',
+      url='',
+      package_dir={'csper':'src'},
+      packages=['csper'],
+      install_requires=["numpy", "astropy", "scipy"],
+      license='GPLv3',
+      classifiers=[
+          "Topic :: Scientific/Engineering",
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python"
+      ]
+     )
